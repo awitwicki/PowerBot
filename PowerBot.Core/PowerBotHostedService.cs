fd40f6990eba8037d202281ce555bd651c8ad368
Andrew Witwicki <vitvitskiy3@gmail.com>
2021-02-13 10:00:05 +0100

    Add PowerBot.Core library

---
+++ PowerBot.Core/PowerBotHostedService.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+
+namespace PowerBot.Core
+{
+    public class PowerBotHostedService : IHostedService
+    {
+        private static TelegramBotClient Bot { get; set; }
+
+        private readonly IServiceProvider _scopeFactory;
+        private readonly IConfiguration _configuration;
+
+        public static bool Started { get; set; } = false;
+
+        public PowerBotHostedService(
+            IServiceProvider scopeFactory,
+            IConfiguration configuration
+            )
+        {
+            _scopeFactory = scopeFactory;
+            _configuration = configuration;
+        }
+
+        public Task StartAsync(CancellationToken stoppingToken)
+        {
+            var token = _configuration["TelegramAccessToken"];
+            if (token == null)
+                throw new InvalidOperationException("Can't find TelegramAccessToken");
+
+            Bot = new TelegramBotClient(token);
+            StartBotAsync();
+            Started = true;
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Bot.StopReceiving();
+
+            return Task.CompletedTask;
+        }
+        public void StartBotAsync()
+        {
+            Bot.OnMessage += BotOnMessageReceived;
+
+            //Start listening
+            Bot.StartReceiving(Array.Empty<UpdateType>());
+        }
+
+        async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+
+            // Exmaple
+            await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.Typing);
+
+            if (message.Text != null)
+                await Bot.SendTextMessageAsync(message.Chat.Id, message.Text);
+        }
+    }
+
+}
---
+++ PowerBot.Core/PowerBotServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PowerBot.Core
+{
+    public static class PowerBotServiceCollectionExtensions
+    {
+        public static IServiceCollection AddPowerBot(this IServiceCollection services)
+        {
+            services.AddHostedService<PowerBotHostedService>();
+            return services;
+        }
+    }
+}
